eba6cc7d5009818a13d492ceb4faa8943ae2eee1
scriptsvistas <scritpsvistas@gmail.com>
2020-02-28 13:03:14 -0500

    Ultimo cambio

--- PKG_RPT_ERRS.sql
+++ PKG_RPT_ERRS.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE PACKAGE                   PKG_RPT_ERRS is
  
   -- Author  : SCASTILLO 
   -- Created : 04/02/2015
-   
+  -- Subir   
      
   TYPE Tr_Columnas_ValError IS RECORD(
     tipoerror           NVARCHAR2(15),
