6c1bd686cc17807a3527850d905605f2bc04746f
scriptsvistas <scritpsvistas@gmail.com>
2020-02-28 13:58:01 -0500

    segunda subida

--- PKG_RPT_ERRS.sql
+++ PKG_RPT_ERRS.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE PACKAGE                   PKG_RPT_ERRS is
  
   -- Author  : SCASTILLO 
   -- Created : 04/02/2015
-  -- Subir   
+  -- Subir 2  
      
   TYPE Tr_Columnas_ValError IS RECORD(
     tipoerror           NVARCHAR2(15),
